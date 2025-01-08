22e9b1b15af5f5ea9d5bb355eb8243d27cf11ed8
Tomas <tomas.vicente.7e5@itb.cat>
2025-01-08 12:21:24 +0100

    pushing local changes to gh for next class

--- RogueLike/Assets/Scripts/MainMenu/ButtonsMethods.cs
+++ RogueLike/Assets/Scripts/MainMenu/ButtonsMethods.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ButtonsMethods : MonoBehaviour
 {
+    public GameObject moveKeys;
     public void PlayGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("BasementMain");
@@ -12,5 +13,10 @@ public class ButtonsMethods : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ShowTutorial()
+    {
+        moveKeys.GetComponent<Animator>().SetBool("Move", true);
     }
 }
